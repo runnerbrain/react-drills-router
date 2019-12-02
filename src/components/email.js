b411949f0f5a76da70784ad365eb5904c404611c
Glitch (react-email-starting-point) <none>
2019-12-02 21:45:21 +0000

    🐆💐 Checkpoint ./src/components/sidebar.js:3110172/1667 ./src/components/email.js:3110172/2325

--- src/components/email.js
+++ src/components/email.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import Sidebar from './sidebar';
-import SingleEmail from './single-email';
-import EmailList from './email-list';
-import './email.css';
+import React from "react";
+import Sidebar from "./sidebar";
+import SingleEmail from "./single-email";
+import EmailList from "./email-list";
+import "./email.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export default function Email() {
-    return (
-        <div className="email">
-            <Sidebar />
-            <main>
-                <EmailList folderId="inbox" />
-                {/* <SingleEmail folderId="inbox" emailId="1" /> */}
-            </main>
-        </div>
-    );
+  return (
+    <Router>
+      <div className="email">
+        <Sidebar />
+        <main>
+          <Route exact path="" render ={<EmailList folderId="inbox" }
+          {/* <SingleEmail folderId="inbox" emailId="1" /> */}
+        </main>
+      </div>
+    </Router>
+  );
 }
