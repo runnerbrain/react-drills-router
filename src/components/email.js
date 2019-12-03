f8d6a2de339f17c1abac7d2697899892e589559b
Glitch (fair-cloudberry) <none>
2019-12-03 01:23:14 +0000

    💘🎒 Checkpoint ./src/components/email.js:3110172/393

--- src/components/email.js
+++ src/components/email.js
@@ -3,7 +3,7 @@ import Sidebar from "./sidebar";
 import SingleEmail from "./single-email";
 import EmailList from "./email-list";
 import "./email.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function Email() {
   return (
@@ -11,10 +11,14 @@ export default function Email() {
       <div className="email">
         <Sidebar />
         <main>
-          <Route exact path="/inbox" render ={ (props) => <EmailList {...props}  folderId="inbox"  /> } />
+          <Switch>
+            <Redirect exact from="/" to="/inbox" />
+            <Route exact path="/:folderId" component={EmailList} />
+          </Switch>
+            { /*<Route exact path="/inbox" render ={ (props) => <EmailList {...props}  folderId="inbox"  /> } />
           <Route exact path="/spam" render ={ (props) => <EmailList {...props}  folderId="spam"  /> } />
-          <Route exact path="/inbox/:emailId" render = {(props) => <SingleEmail {...props} emailId={1} /> } />
-          {/* <SingleEmail folderId="inbox" emailId="1" /> */}
+          <Route exact path="/inbox/:emailId" render = {(props) => <SingleEmail {...props} emailId={1} /> } /> 
+          {/* <SingleEmail folderId="inbox" emailId="1" />  */}
         </main>
       </div>
     </Router>
