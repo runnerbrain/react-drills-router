37890a7811a1a4bd4941529ace87822983bbc9a8
Glitch (fair-cloudberry) <none>
2019-12-03 00:24:20 +0000

    🍎🎃 Checkpoint ./src/components/email.js:3110172/3 ./src/components/email-list.js:3110172/68

--- src/components/email-list.js
+++ src/components/email-list.js
@@ -10,7 +10,7 @@ export function EmailList(props) {
                 {email.from}
             </div>
             <div className="email-list-email-title">
-                <Link to={`${email.id}`}>{email.title}</Link>
+                <Link to={`${props.folderName}/${email.id}`}>{email.title}</Link>
             </div>
         </li>
     );
